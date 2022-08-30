2256e6c43046ce499e43104186a927a2ef91d3d1
Azima Keshwani <azima786@gmail.com>
2022-08-30 12:14:59 -0500

    starting project

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,4 +1,9 @@
+/// <reference types = "Cypress" />
+
 describe('Test Contact US Form via WebDriverUni', () => {
-  it('Should be able to submit a successful submission via contact us form', () => {})
+  it('Should be able to submit a successful submission via contact us form', () => {
+    cy.visit('https://webdriveruniversity.com/')
+    cy.get('#contact-us > .thumbnail > .section-title > h1').click()
+  })
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {})
 })
