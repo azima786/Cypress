e4cc61283b4156ad5c5ad85bf16ea389ab62b869
Azima Keshwani <azima786@gmail.com>
2022-08-30 11:52:21 -0500

    starting project

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,4 @@
+describe('Test Contact US Form via WebDriverUni', () => {
+  it('Should be able to submit a successful submission via contact us form', () => {})
+  it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {})
+})
