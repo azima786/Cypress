e6ee82225f4bc8893f444011258322fcc741481b
Azima Keshwani <azima786@gmail.com>
2022-08-30 14:57:11 -0500

    add test case

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -2,8 +2,19 @@
 
 describe('Test Contact US Form via WebDriverUni', () => {
   it('Should be able to submit a successful submission via contact us form', () => {
-    cy.visit('https://webdriveruniversity.com/')
-    cy.get('#contact-us > .thumbnail > .section-title > h1').click()
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    //cy.get('#contact-us').click({ force: true })
+    cy.get('[name="first_name"]').type('AZIMA')
+    cy.get('[name="last_name"]').type('KESH')
+    cy.get('[name="email"]').type('KESH@gmail.com')
+    cy.get('[name="message"]').type('hello')
+    cy.get('[type="submit"]').click()
   })
-  it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {})
+  it.only('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    cy.get('[name="first_name"]').type('AZIMA')
+    cy.get('[name="last_name"]').type('KESH')
+    cy.get('[name="message"]').type('hello')
+    cy.get('[type="submit"]').click()
+  })
 })
