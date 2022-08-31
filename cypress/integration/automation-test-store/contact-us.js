be32700c5070e174d67662198b35169b22bb2d6d
Azima Keshwani <azima786@gmail.com>
2022-08-31 14:09:31 -0500

    added assertions

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -6,7 +6,12 @@ describe('Test Contact US Form via automation test store', () => {
     cy.get('a[href$="contact"]').click()
     cy.get('#ContactUsFrm_first_name').type('AZIMA')
     cy.get('#ContactUsFrm_email').type('azima@gmail.com')
+    cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
     cy.get('#ContactUsFrm_enquiry').type('Learning cypress')
     cy.get('button[title="Submit"]').click()
+    cy.get('.mb40 > :nth-child(3)').should(
+      'have.text',
+      'Your enquiry has been successfully sent to the store owner!',
+    )
   })
 })
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -9,12 +9,15 @@ describe('Test Contact US Form via WebDriverUni', () => {
     cy.get('[name="email"]').type('KESH@gmail.com')
     cy.get('[name="message"]').type('hello')
     cy.get('[type="submit"]').click()
+    cy.get('h1').should('have.text', 'Thank You for your Message!')
+    cy.get('h1').should('not.have.text', '2Thank You for your Message!')
   })
-  it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
+  it.only('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
     cy.get('[name="first_name"]').type('AZIMA')
     cy.get('[name="last_name"]').type('KESH')
     cy.get('[name="message"]').type('hello')
     cy.get('[type="submit"]').click()
+    cy.get('body').contains('Error: all fields are required')
   })
 })
