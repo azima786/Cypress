102b2e82e894f63cbc3bd251b73b99fac78b1867
Azima Keshwani <azima786@gmail.com>
2022-09-01 15:38:37 -0500

    update

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -9,7 +9,7 @@ describe('Alias and invoke', () => {
     cy.get('@productThumbnail').should('include', 'Seaweed Conditioner')
   })
 
-  it.only('Validate product thumbnail', () => {
+  it('Validate product thumbnail', () => {
     cy.visit('https://automationteststore.com/')
     cy.get('.thumbnail').as('productThumbnail')
     cy.get('@productThumbnail').should('have.length', 16)
@@ -17,5 +17,47 @@ describe('Alias and invoke', () => {
       .find('.productcart')
       .invoke('attr', 'title')
       .should('include', 'Add to Cart')
+  })
+
+  it.only('Calculate total of normal and sale products', () => {
+    cy.visit('https://automationteststore.com/')
+    cy.get('.thumbnail').as('productThumbnail')
+    //cy.get('@productThumbnail')
+    //  .find('.oneprice')
+    //  .each(($el, index, $list) => {
+    //    cy.log($el.text())
+    //  })
+    cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice')
+    cy.get('.thumbnail').find('.pricenew').invoke('text').as('saleItemPrice')
+
+    var itemsTotal = 0
+    cy.get('@itemPrice').then(($linkText) => {
+      var itemPriceTotal = 0
+      var itemPrice = $linkText.split('$')
+      var i
+      for (i = 0; i < itemPrice.length; i++) {
+        cy.log(itemPrice[i])
+        itemPriceTotal += Number(itemPrice[i])
+      }
+      itemsTotal += itemPriceTotal
+      cy.log('Non sale price total ' + itemPriceTotal)
+    })
+
+    cy.get('@saleItemPrice')
+      .then(($linkText) => {
+        var saleItemPrice = 0
+        var saleItemPrice = $linkText.split('$')
+        var i
+        for (i = 0; i < saleItemPrice.length; i++) {
+          cy.log(saleItemPrice[i])
+          saleItemPrice += Number(saleItemPrice[i])
+        }
+        itemsTotal += saleItemPrice
+        cy.log('Sale price total ' + saleItemPrice)
+      })
+      .then(() => {
+        cy.log('Total price of all product : ' + itemsTotal)
+        expect(itemsTotal).to.equal(572.45)
+      })
   })
 })
