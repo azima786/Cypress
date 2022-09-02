079e7138e20070cde04e27ecab812b9d38b017a5
Azima Keshwani <azima786@gmail.com>
2022-09-02 10:20:47 -0500

    update

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,9 @@
 
 describe('Test Contact US Form via WebDriverUni', () => {
   it('Should be able to submit a successful submission via contact us form', () => {
-    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    // cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    cy.visit('https://webdriveruniversity.com/')
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true }) // to open in same browser tab
     //cy.get('#contact-us').click({ force: true })
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
     cy.title().should('include', 'WebDriver | Contact Us')
@@ -16,7 +18,9 @@ describe('Test Contact US Form via WebDriverUni', () => {
     cy.get('h1').should('not.have.text', '2Thank You for your Message!')
   })
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    // cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    cy.visit('https://webdriveruniversity.com')
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
     cy.get('[name="first_name"]').type('AZIMA')
     cy.get('[name="last_name"]').type('KESH')
     cy.get('[name="message"]').type('hello')
